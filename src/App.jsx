4c7a8213610505464581a228fe050563ffd2bd59
Enifome Alalor <fomealalor@gmail.com>
2024-01-22 10:27:07 +0100

    done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from './component/NavBar'
+import Body from './component/Body'
+import Footer from './component/Footer'
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Body />
+    <Footer /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Body.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <>
+          <header className="App-header">
+        <h1>Welcome to My App</h1>
+      </header>
+
+      <div className="card-container">
+        <div className="card">
+          <h2>Card 1</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate et harum sunt laborum eos accusamus dolorem quidem quibusdam, veritatis praesentium ad nobis impedit aliquam earum atque qui voluptatem tempora assumenda.</p>
+        </div>
+        <div className="card">
+          <h2>Card 2</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis inventore nulla cumque quaerat eos adipisci velit at aut quibusdam, aperiam reprehenderit nesciunt voluptatibus aliquam molestias praesentium fuga labore illum voluptas..</p>
+        </div>
+        <div className="card">
+          <h2>Card 3</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consectetur enim sunt harum labore itaque illum, earum ab ea modi iure voluptatibus porro odit exercitationem architecto quas dicta, nulla commodi.</p>
+        </div>
+      </div>
+</>
+  )
+}
+
+export default Body
